7d8c8f440c74e5a255a8341f08824ba753f54795
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-09-21 23:21:23 +0700

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="shadow-sm ...">abc</div>
+        <div className="shadow ...">abc</div>
+        <div className="shadow-md ...">abc</div>
+        <div className="shadow-lg ...">abc</div>
+        <div className="shadow-xl ...">abc</div>
+        <div className="shadow-2xl ...">abc</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
